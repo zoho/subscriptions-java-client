fcad5361c1a7425409e5a3bef410ebf736d57be7
lavanya.cm <lavanya.cm@zohocorp.com>
2022-03-09 17:11:06 +0530

    Plan class is updated

--- src/main/java/com/zoho/zs/client/api/model/Plan.java
+++ src/main/java/com/zoho/zs/client/api/model/Plan.java
@@ -16,8 +16,7 @@ import com.zoho.zs.client.api.net.Resource;
 import com.zoho.zs.client.api.net.ResourceUtil;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Plan extends Resource
-{
+public class Plan extends Resource {
 	String planCode;
 
 	String name;
@@ -48,6 +47,12 @@ public class Plan extends Resource
 
 	String taxExemptionCode;
 
+	Boolean showInWidget;
+
+	Boolean showInPortal;
+
+	String Description;
+
 	@JsonIgnore
 	String taxName;
 
@@ -72,288 +77,463 @@ public class Plan extends Resource
 	@JsonIgnore
 	List<Addon> addons;
 
-	public static Plan retrieve(String planCode) throws ZSAPIException
-	{
+	List<Plan.CustomField> customFields;
+
+	List<Plan.Tag> tags;
+
+	public static Plan retrieve(String planCode) throws ZSAPIException {
 		return ResourceUtil.process(RequestMethod.GET, instancePath(Plan.class, planCode), Plan.class);
 	}
 
-	public static ListResponse<Plan> list() throws ZSAPIException
-	{
+	public static ListResponse<Plan> list() throws ZSAPIException {
 		return list(GenericListParams.getDefaults());
 	}
 
-	public static ListResponse<Plan> list(GenericListParams params) throws ZSAPIException
-	{
+	public static ListResponse<Plan> list(GenericListParams params) throws ZSAPIException {
 		return ResourceUtil.list(RequestMethod.GET, classPath(Plan.class), Plan.class, params);
 	}
 
-	public static Plan create(Plan plan) throws ZSAPIException
-	{
+	public static Plan create(Plan plan) throws ZSAPIException {
 		return ResourceUtil.process(RequestMethod.POST, classPath(Plan.class), Plan.class, plan);
 	}
 
-	public static void markAsActive(String planCode) throws ZSAPIException
-	{
+	public static void markAsActive(String planCode) throws ZSAPIException {
 		ResourceUtil.process(RequestMethod.POST, instancePath(Plan.class, planCode) + "/markasactive", Plan.class);
 	}
 
-	public static void markAsInactive(String planCode) throws ZSAPIException
-	{
+	public static void markAsInactive(String planCode) throws ZSAPIException {
 		ResourceUtil.process(RequestMethod.POST, instancePath(Plan.class, planCode) + "/markasinactive", Plan.class);
 	}
 
-	public static Plan update(Plan plan, String planCode) throws ZSAPIException
-	{
+	public static Plan update(Plan plan, String planCode) throws ZSAPIException {
 		return ResourceUtil.process(RequestMethod.PUT, instancePath(Plan.class, planCode), Plan.class, plan);
 	}
 
-	public static void delete(String planCode) throws ZSAPIException
-	{
+	public static void delete(String planCode) throws ZSAPIException {
 		ResourceUtil.process(RequestMethod.DELETE, instancePath(Plan.class, planCode), Plan.class);
 	}
 
-	public void setPlanCode(String planCode)
-	{
+	public void setPlanCode(String planCode) {
 		this.planCode = planCode;
 	}
 
-	public String getPlanCode()
-	{
+	public String getPlanCode() {
 		return planCode;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setProductId(String productId)
-	{
+	public void setProductId(String productId) {
 		this.productId = productId;
 	}
 
-	public String getProductId()
-	{
+	public String getProductId() {
 		return productId;
 	}
 
-	public void setProductType(String productType)
-	{
+	public void setProductType(String productType) {
 		this.productType = productType;
 	}
 
-	public String getProductType()
-	{
+	public String getProductType() {
 		return productType;
 	}
 
-	public void setTaxId(String taxId)
-	{
+	public void setTaxId(String taxId) {
 		this.taxId = taxId;
 	}
 
-	public String getTaxId()
-	{
+	public String getTaxId() {
 		return taxId;
 	}
 
 	@JsonProperty
-	public void setTaxName(String taxName)
-	{
+	public void setTaxName(String taxName) {
 		this.taxName = taxName;
 	}
 
 	@JsonIgnore
-	public String getTaxName()
-	{
+	public String getTaxName() {
 		return taxName;
 	}
 
 	@JsonProperty
-	public void setTaxType(String taxType)
-	{
+	public void setTaxType(String taxType) {
 		this.taxType = taxType;
 	}
 
 	@JsonIgnore
-	public String getTaxType()
-	{
+	public String getTaxType() {
 		return taxType;
 	}
 
-	public void setAccountId(String accountId)
-	{
+	public void setAccountId(String accountId) {
 		this.accountId = accountId;
 	}
 
-	public String getAccountId()
-	{
+	public String getAccountId() {
 		return accountId;
 	}
 
-	public void setTrialPeriod(Integer trialPeriod)
-	{
+	public void setTrialPeriod(Integer trialPeriod) {
 		this.trialPeriod = trialPeriod;
 	}
 
-	public Integer getTrialPeriod()
-	{
+	public Integer getTrialPeriod() {
 		return trialPeriod;
 	}
 
-	public void setSetupFee(BigDecimal setupFee)
-	{
+	public void setSetupFee(BigDecimal setupFee) {
 		this.setupFee = setupFee;
 	}
 
-	public BigDecimal getSetupFee()
-	{
+	public BigDecimal getSetupFee() {
 		return setupFee;
 	}
 
-	public void setRecurringPrice(BigDecimal recurringPrice)
-	{
+	public void setRecurringPrice(BigDecimal recurringPrice) {
 		this.recurringPrice = recurringPrice;
 	}
 
-	public BigDecimal getRecurringPrice()
-	{
+	public BigDecimal getRecurringPrice() {
 		return recurringPrice;
 	}
 
-	public void setInterval(Integer interval)
-	{
+	public void setInterval(Integer interval) {
 		this.interval = interval;
 	}
 
-	public Integer getInterval()
-	{
+	public Integer getInterval() {
 		return interval;
 	}
 
-	public void setIntervalUnit(String intervalUnit)
-	{
+	public void setIntervalUnit(String intervalUnit) {
 		this.intervalUnit = intervalUnit;
 	}
 
-	public String getIntervalUnit()
-	{
+	public String getIntervalUnit() {
 		return intervalUnit;
 	}
 
-	public void setBillingCycles(Integer billingCycles)
-	{
+	public void setBillingCycles(Integer billingCycles) {
 		this.billingCycles = billingCycles;
 	}
 
-	public Integer getBillingCycles()
-	{
+	public Integer getBillingCycles() {
 		return billingCycles;
 	}
 
 	@JsonProperty
-	public void setPlanId(String planId)
-	{
+	public void setPlanId(String planId) {
 		this.planId = planId;
 	}
 
 	@JsonIgnore
-	public String getPlanId()
-	{
+	public String getPlanId() {
 		return planId;
 	}
 
 	@JsonProperty
-	public void setStatus(String status)
-	{
+	public void setStatus(String status) {
 		this.status = status;
 	}
 
 	@JsonIgnore
-	public String getStatus()
-	{
+	public String getStatus() {
 		return status;
 	}
 
 	@JsonProperty
-	public void setCreatedTime(Date createdTime)
-	{
+	public void setCreatedTime(Date createdTime) {
 		this.createdTime = createdTime;
 	}
 
 	@JsonIgnore
-	public Date getCreatedTime()
-	{
+	public Date getCreatedTime() {
 		return createdTime;
 	}
 
 	@JsonProperty
-	public void setUpdatedTime(Date updatedTime)
-	{
+	public void setUpdatedTime(Date updatedTime) {
 		this.updatedTime = updatedTime;
 	}
 
 	@JsonIgnore
-	public Date getUpdatedTime()
-	{
+	public Date getUpdatedTime() {
 		return updatedTime;
 	}
 
-	public void setIsTaxable(Boolean isTaxable)
-	{
+	public void setIsTaxable(Boolean isTaxable) {
 		this.isTaxable = isTaxable;
 	}
 
-	public Boolean getIsTaxable()
-	{
+	public Boolean getIsTaxable() {
 		return isTaxable;
 	}
 
-	public void setTaxExemptionCode(String taxExemptionCode)
-	{
+	public void setTaxExemptionCode(String taxExemptionCode) {
 		this.taxExemptionCode = taxExemptionCode;
 	}
 
-	public String getTaxExemptionId()
-	{
+	public String getTaxExemptionId() {
 		return taxExemptionId;
 	}
 
-	public void setTaxExemptionId(String taxExemptionId)
-	{
+	public void setTaxExemptionId(String taxExemptionId) {
 		this.taxExemptionId = taxExemptionId;
 	}
 
-	public String getTaxPercentage()
-	{
+	public String getTaxPercentage() {
 		return taxPercentage;
 	}
 
-	public void setTaxPercentage(String taxPercentage)
-	{
+	public void setTaxPercentage(String taxPercentage) {
 		this.taxPercentage = taxPercentage;
 	}
 
-	public String getTaxExemptionCode()
-	{
+	public String getTaxExemptionCode() {
 		return taxExemptionCode;
 	}
 
+	public void setShowInWidget(Boolean showInWidget) {
+		this.showInWidget = showInWidget;
+	}
+
+	public Boolean getShowInWidget() {
+		return this.showInWidget;
+	}
+
+	public void setShowInPortal(Boolean showInPortal) {
+		this.showInPortal = showInPortal;
+	}
+
+	public Boolean getShowInPortal() {
+		return this.showInPortal;
+	}
+
+	public void setDescription(String Description) {
+		this.Description = Description;
+	}
+
+	public String getDescription() {
+		return this.Description;
+	}
+
 	@JsonProperty
-	public void setAddons(List<Addon> addons)
-	{
+	public void setAddons(List<Addon> addons) {
 		this.addons = addons;
 	}
 
 	@JsonIgnore
-	public List<Addon> getAddons()
-	{
+	public List<Addon> getAddons() {
 		return addons;
 	}
 
+	public void setCustomFields(List<Plan.CustomField> customFields) {
+		this.customFields = customFields;
+	}
+
+	public List<Plan.CustomField> getCustomFields() {
+		return customFields;
+	}
+
+	public void setTags(List<Plan.Tag> tags){this.tags=tags;}
+
+	public List<Plan.Tag> getTags(){return tags;}
+
+	public static class CustomField extends Resource {
+		String customFieldId;
+
+		String value;
+
+		String label;
+
+		String showInStore;
+
+		String showInPortal;
+
+		String placeholder;
+
+		String isActive;
+
+		String index;
+
+		Boolean showOnPdf;
+
+		Boolean editOnPortal;
+
+		Boolean editOnStore;
+
+		Boolean showInAllPdf;
+
+		String valueFormatted;
+
+		String searchEntity;
+
+		String dataType;
+
+		Boolean isDependentField;
+
+		public void setValueFormatted(String valueFormatted) {
+			this.valueFormatted = valueFormatted;
+		}
+
+		public String getValueFormatted() {
+			return valueFormatted;
+		}
+
+		public void setSearchEntity(String searchEntity) {
+			this.searchEntity = searchEntity;
+		}
+
+		public String getSearchEntity() {
+			return searchEntity;
+		}
+
+		public void setDataType(String dataType) {
+			this.dataType = dataType;
+		}
+
+		public String getDataType() {
+			return dataType;
+		}
+
+		public void setShowOnPdf(Boolean showOnPdf) {
+			this.showOnPdf = showOnPdf;
+		}
+
+		public Boolean getShowOnPdf() {
+			return showOnPdf;
+		}
+
+		public void setEditOnPortal(Boolean editOnPortal) {
+			this.editOnPortal = editOnPortal;
+		}
+
+		public Boolean getEditOnPortal() {
+			return editOnPortal;
+		}
+
+		public void setEditOnStore(Boolean editOnStore) {
+			this.editOnStore = editOnStore;
+		}
+
+		public Boolean getEditOnStore() {
+			return editOnStore;
+		}
+
+		public void setShowInAllPdf(Boolean showInAllPdf) {
+			this.showInAllPdf = showInAllPdf;
+		}
+
+		public Boolean getShowInAllPdf() {
+			return showInAllPdf;
+		}
+
+		public void setIsDependentField(Boolean isDependentField) {
+			this.isDependentField = isDependentField;
+		}
+
+		public Boolean getIsDependentField() {
+			return isDependentField;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setLabel(String label) {
+			this.label = label;
+		}
+
+		public String getLabel() {
+			return label;
+		}
+
+		public String getCustomfieldId() {
+			return customFieldId;
+		}
+
+		public void setCustomfieldId(String customFieldId) {
+			this.customFieldId = customFieldId;
+		}
+
+		public void setShowInStore(String showInStore) {
+			this.showInStore = showInStore;
+		}
+
+		public String getShowInStore() {
+			return showInStore;
+		}
+
+		public void setShowInPortal(String showInPortal) {
+			this.showInPortal = showInPortal;
+		}
+
+		public String getShowInPortal() {
+			return showInPortal;
+		}
+
+		public void setPlaceholder(String placeholder) {
+			this.placeholder = placeholder;
+		}
+
+		public String getPlaceholder() {
+			return placeholder;
+		}
+
+		public void setIsActive(String isActive) {
+			this.isActive = isActive;
+		}
+
+		public String getIsActive() {
+			return isActive;
+		}
+
+		public void setIndex(String index) {
+			this.index = index;
+		}
+
+		public String getIndex() {
+			return index;
+		}
+
+	}
+
+	public static class Tag {
+		String tagOptionId;
+
+		Boolean isTagMandatory;
+
+		String tagName;
+
+		String tagId;
+
+		String tagOptionName;
+
+		public void setTagOptionId(String tagOptionId) {this.tagOptionId = tagOptionId;}
+		public String getTagOptionId() {return tagOptionId;}
+
+		public void setTagName(String tagName) {this.tagName = tagName;}
+		public String getTagName() {return tagName;}
+
+		public void setTagId(String tagId) {this.tagId = tagId;}
+		public String getTagId() {return tagId;}
+
+		public void setTagOptionName(String tagOptionName) {this.tagOptionName= tagOptionName;}
+		public String getTagOptionName() {return tagOptionName;}
+
+		public void setIsTagMandatory(Boolean isTagMandatory){this.isTagMandatory = isTagMandatory;}
+		public Boolean getIsTagMandatory(){return isTagMandatory;}
+	}
 }
